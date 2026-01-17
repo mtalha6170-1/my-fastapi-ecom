28879b6c08c7ed9fb5f78d9614a4e4c01774f2e3
Muhammad Talha <mtalha6170@gmail.com>
2026-01-17 22:32:16 +0500

    FastAPI e-commerce API

--- final_app/main.py
+++ final_app/main.py
@@ -20,3 +20,6 @@ def read_products(db:Session = Depends(get_db)):
 @app.post("/products", response_model=schemas.Product)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
     return crud.create_product(db=db, product=product)
+
+
+
